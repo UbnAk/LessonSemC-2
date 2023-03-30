87ff0702bee9448b24248ced0318efa1f9c5e306
Maksim Akimov <maksimakimov.96@mail.ru>
2023-03-30 15:09:05 +0300

    Add Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int i = GetInt();
+int second = PrintSecond(i);
+System.Console.WriteLine(second);
+
+int GetInt()
+{
+System.Console.WriteLine("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+return a;
+}
+
+int PrintSecond(int number)
+{
+    int a = number % 100;
+    int b = number % 10;
+    int c = (a - b) / 10;
+    return c;
+}
